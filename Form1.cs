254c45061ee879f563d977e2f7cfa15bb22fab85
WIN_NT\x16j053 <x16j053@chiba-fjb.ac.jp>
2017-06-02 11:54:03 +0900

    ボタンによる移動

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        int speed = 10;
+        int moveX;
+        int moveY;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moveX = 0;
+            moveY = -speed;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            moveX = 0;
+            moveY = speed;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            moveY = 0;
+            moveX = speed;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            moveY = 0;
+            moveX = -speed;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += moveX;
+            pictureBox1.Top += moveY;
+        }
+    }
+}
